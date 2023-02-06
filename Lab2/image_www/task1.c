5f4f46529d739527aa01ce4cfa536ca387a7c135
Naman Choudhary <appleinc2341@gmail.com>
2023-02-06 13:59:13 +0530

    Lab2

---
+++ Lab2/image_www/task1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[], char* envp[])
+{
+	setuid(geteuid());
+	system("/bin/ls -l");
+	return 0;
+}
